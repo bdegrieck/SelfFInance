e768743554297c4f8d14bb388fffd1d9232b5ba5
Ben DeGrieck <bendegrieck@comcast.net>
2024-03-24 11:57:50 -0400

    format

--- BackEnd/constants.py
+++ BackEnd/constants.py
@@ -1,2 +1,2 @@
-API_KEY = "CRU63X7J4COJ46F2" # api key for alpha vantage
 SOURCE = "https://www.alphavantage.co/documentation/" # API source
+API_KEY = "CRU63X7J4COJ46F2" # api key for alpha vantage
--- main.py
+++ main.py
@@ -4,11 +4,13 @@ from Website import create_app
 
 app = create_app()
 
+
 # Formats float values to strings with commas to look like money
 def format_currency(value):
     if value == "None":
         return "0".format(value)
     return "{:,.2f}".format(value)
+
 
 app.jinja_env.filters['format_currency'] = format_currency
 
