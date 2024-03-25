2f36f802f0912a0b30efb12c549ce9d8f15fc979
Ben DeGrieck <bendegrieck@comcast.net>
2024-03-25 12:45:56 -0400

    testing

---
+++ Tests/test_CompanyData.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from BackEnd.companyData import get_ticker_balance_df_adj, CompanyData
+
+
+class TestCompanyData:
+
+    # tests the cash flow calculation operation
+    def test_ticker_balance_adjusted(self):
+        balance_sheet = pd.DataFrame({
+            "Operating Cash Flow": [100],
+            "Cash Flow From Financing": [-50],
+            "Cash Flow From Investment": [25],
+            "Total Revenue": [0],
+            "Profit": [0],
+        })
+        balance_sheet = get_ticker_balance_df_adj(balance_df=balance_sheet)
+        assert balance_sheet["Cash Flow"].iloc[0] == 75
+
+    def test_company_data_grab(self):
+        ticker = "NVDA"
+        nvidia_data = CompanyData(ticker=ticker)
+
+        nvidia_data_dfs = nvidia_data.ticker_df_data
+
+        # check values are not less than 0 in ticker prices in Open, High, Close, Volume
+        ticker_prices = nvidia_data_dfs["ticker_prices_df"]
+        for df_name in ticker_prices:
+            assert ticker_prices[ticker_prices[df_name] < 0].empty
+
+        # check if values are greater than 0 for market cap, 52 week high, 52 week low
+        ticker_overview = nvidia_data_dfs["ticker_overview_df"]
+        assert ticker_overview["Market Cap"].iloc[0] > 0
+        assert ticker_overview["52 Week High"].iloc[0] > 0
+        assert ticker_overview["52 Week Low"].iloc[0] > 0
+
+    def test_company_data_grab_filtration(self):
+        ticker = "LULU"
+        lulu_data = CompanyData(ticker=ticker).ticker_df_data
+
+        # checks if values are not null or 'None'
+        for df_name, df in lulu_data.items():
+            for column in df:
+                assert df[column].isna().sum() == 0
+                assert df[df[column] == "None"].empty
---
+++ Tests/test_Errors.py
@@ -0,0 +1,74 @@
+from BackEnd.errors import get_formatted_ticker, valid_ticker_input, check_same_tickers
+
+
+class TestErrors:
+
+    def test_user_valid_inputs(self):
+
+        # test company full name
+        ticker = "microsoft"
+
+        assert valid_ticker_input(ticker=ticker) is None
+        assert get_formatted_ticker(ticker=ticker) == "MSFT"
+
+        # test space after input
+        ticker = "apple "
+
+        assert valid_ticker_input(ticker=ticker) is None
+        assert get_formatted_ticker(ticker=ticker) == "AAPL"
+
+        # test lower case ticker
+        ticker = "tsla"
+
+        assert valid_ticker_input(ticker=ticker) is None
+        assert get_formatted_ticker(ticker=ticker) == "TSLA"
+
+        # test upper case ticker
+        ticker = "NVDA"
+
+        assert valid_ticker_input(ticker=ticker) is None
+        assert get_formatted_ticker(ticker=ticker) == "NVDA"
+
+        # test ETF
+        ticker = "QQQ"
+
+        assert valid_ticker_input(ticker=ticker) is None
+        assert get_formatted_ticker(ticker=ticker) == "QQQ"
+
+    def test_check_same_tickers_input(self):
+        # test if user enters two of the same tickers which informs user to re enter a new ticker
+        ticker1 = "AAPL"
+        ticker2 = "AAPL"
+
+        assert valid_ticker_input(ticker=ticker1) is None
+        assert valid_ticker_input(ticker=ticker2) is None
+
+        ticker1 = get_formatted_ticker(ticker1)
+        ticker2 = get_formatted_ticker(ticker2)
+
+        assert check_same_tickers(ticker1=ticker1, ticker2=ticker2) == "Please input two different tickers"
+
+        # test if user enters two of the same tickers but with company name and ticker symbol
+        ticker1 = "delta air lines"
+        ticker2 = "DAL"
+
+        assert valid_ticker_input(ticker=ticker1) is None
+        assert valid_ticker_input(ticker=ticker2) is None
+
+        ticker1 = get_formatted_ticker(ticker1)
+        ticker2 = get_formatted_ticker(ticker2)
+
+        assert check_same_tickers(ticker1=ticker1, ticker2=ticker2) == "Please input two different tickers"
+
+    def test_invalid_tickers(self):
+        # test invalid ticker
+        ticker = "sfhasdklfhalksgh"
+        assert valid_ticker_input(ticker=ticker) == "Your inputted Ticker does not exist"
+
+        # test company that went private (twitter went private before being bought by Elon)
+        ticker = "TWTR"
+        assert valid_ticker_input(ticker=ticker) == "Your inputted Ticker does not exist"
+
+        # test non american  ticker which is invalid
+        ticker = "MMIG.F"
+        assert valid_ticker_input(ticker=ticker) == "Your inputted Ticker does not exist"
