601c7a6a7acbbee38ba12a93a9ee5099bc58242c
Ben DeGrieck <bendegrieck@comcast.net>
2024-03-25 20:10:08 -0400

    port change

--- main.py
+++ main.py
@@ -16,4 +16,4 @@ app.jinja_env.filters['format_currency'] = format_currency
 
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'),
-            port=int(os.getenv('PORT', 4444)))
+            port=int(os.getenv('PORT', 4443)))
