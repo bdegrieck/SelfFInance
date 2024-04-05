e2e08970dff16757d38792a2422d02f5bcf646f0
bdegrieck <92237169+bdegrieck@users.noreply.github.com>
2024-04-04 20:15:39 -0400

    index

--- Website/views.py
+++ Website/views.py
@@ -12,7 +12,7 @@ views = Blueprint("views", __name__)
 
 @views.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 
 @views.route("/tickerdata", methods=["POST"])
