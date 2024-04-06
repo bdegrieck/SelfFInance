59b1cfba6ae4719ec0ec8f4ad1bd1c0af9867be0
bdegrieck <92237169+bdegrieck@users.noreply.github.com>
2024-04-05 20:28:36 -0400

    company name change error

--- BackEnd/errors.py
+++ BackEnd/errors.py
@@ -5,22 +5,13 @@ from BackEnd import constants
 # tickers that the api cannot understand when inputting full company name instead of ticker
 def check_extraneous_tickers(input_name: str):
     extraneous_tickers = {
-        "microsoft": "MSFT",
         "apple": "AAPL",
-        "tesla": "TSLA",
         "target": "TGT",
         "google": "GOOGL",
         "disney": "DIS",
-        "snowflake": "SNOW",
-        "crowdstrike": "CRWD",
-        "netflix": "NFLX",
         "at&t": "T",
-        "gamestop": "GME",
-        "draftkings": "DKNG",
-        "berkshire hathaway": "BRK",
         "visa": "V",
-        "boeing": "BA",
-        "general electric": "GE"
+        "berkshire hathaway": "BRK",
     }
     if input_name in extraneous_tickers.keys():
         return extraneous_tickers[input_name]
@@ -28,17 +19,27 @@ def check_extraneous_tickers(input_name: str):
 
 # formats user inputted ticker to most relevant search done by api
 def get_formatted_ticker(ticker: str) -> str:
+    formatted_ticker = None
     url = f'https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords={ticker}&apikey={constants.API_KEY}'
     ticker_list = requests.get(url).json()
+
+    if len(ticker_list["bestMatches"]) == 0:
+        return f'Your input "{ticker}" does not exist'
+
     name_input = check_extraneous_tickers(input_name=ticker.lower().strip())
     if name_input:
         return name_input
-    elif len(ticker_list["bestMatches"]) == 0:
-        return f'Enter ticker instead of "{ticker}"'
-    elif len(ticker_list["bestMatches"][0]["1. symbol"]) > 5:
-        return f'Enter ticker instead of "{ticker}"'
+
+    # filter out invalid tickers
+    better_matches = []
+    for match in range(0, len(ticker_list["bestMatches"])):
+        if "." not in ticker_list["bestMatches"][match]["1. symbol"] and "-" not in ticker_list["bestMatches"][match]["1. symbol"]:
+            better_matches.append(ticker_list["bestMatches"][match]["1. symbol"])
+
+    if len(better_matches) == 0:
+        return f'Please enter ticker instead of "{ticker}"'
     else:
-        return ticker_list["bestMatches"][0]["1. symbol"]
+        return better_matches[0]
 
 
 # checks user input for a valid ticker that api can detect
