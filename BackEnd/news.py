e9b017c74d98d8884dc29fa3b2ea1d95b24ee324
Ben DeGrieck <bendegrieck@comcast.net>
2024-04-12 18:44:58 -0400

    news fix again

--- BackEnd/news.py
+++ BackEnd/news.py
@@ -14,6 +14,8 @@ class News:
     def get_news(self) -> str:
         try:
             news_link = requests.get(url=self.url).json()
+            if news_link["information"]:
+                return " "
             news_feed = news_link["feed"]
             # checks for a more relevant article with ticker in title
             for news_index in range(len(news_feed)):
